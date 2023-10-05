ebddc964debb29787d8a2c2aefc5ba0e13e9274c
Hotturtle1 <ahmadmustafa287@yahoo.com>
2023-10-05 19:25:46 +0300

    meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+
 import './globals.css'
 import type { Metadata } from 'next'
 import Container from './Components/Container/Container'
@@ -21,18 +22,7 @@ export default function RootLayout({
       </head>
 
       <body >
-      <Helmet>
-                <meta charSet="utf-8" />
-                <title>RedAnime - لمشاهدة الانمي المترجم</title>
-                <meta name="description" content="RedAnime - لمشاهدة الانمي المترجم اون لاين"/>
-                <meta property="og:locale" content="ar_AR"/>
-                <meta property="og:type" content="website"></meta>
-                <meta property="og:title" content="RedAnime"></meta>
-                <meta property="og:description" content="RedAnime - لمشاهدة الانمي المترجم اون لاين"/>
-                <meta property="og:url" content="https://redanime.net"></meta>
-                <meta property="og:site_name" content="RedAnime"/>
-                <meta name="keywords" content="انمي,ملخص انمي,انمي جديد,ملخصات انمي,تلخيص انمي,بتاع انمي,انمي حب,افلام انمي,مسلسلات انمي,انمي اكشن,انمي رومانسي,انمي في السريع,تلخيصات انمي,ملخص فيلم انمي,ملخص انمي كامل,انمي دراما,انمي مدرسي,بتاع انمي طوكيو غول,انميات اكشن,انمي كامل,ملخصات انميات,انمي رومنسي,بتاع انمي هجوم العمالقه,قصة انمي,انميات حب,انميات رومانسيه,انمي بينو,مضحك انمي,افضل انمي,انميات,انمي ناروتو,ملخص فلم انمي,انمي قديم,انمي هجوم العمالقة,ملخص انمي جديد"></meta>
-            </Helmet>
+   
         <Container>
         {children}
         </Container>
