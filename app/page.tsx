5ce70741fc99fa4cbbcbc5ba311c071e7fe079b1
Hotturtle1 <ahmadmustafa287@yahoo.com>
2023-10-05 19:19:40 +0300

    meta tag

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,7 @@ import {Helmet} from "react-helmet";
 export default function Home() {
   return (
     <>
-    <Helmet>
-                <meta charSet="utf-8" />
-                <title>RedAnime - لمشاهدة الانمي المترجم</title>
-               
-            </Helmet>
+
     <main>
         <Hero/>
         <Animejumptron  title="الانميات المثبتة" />
